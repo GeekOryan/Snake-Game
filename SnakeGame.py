81668abe0e65d91f78a2f36f4d48ecc99ffe5511
GeekOryan <225357530+GeekOryan@users.noreply.github.com>
2026-07-17 15:00:28 +0200

    Add food spawning , collision detection and snake growth mechanic

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -8,10 +9,18 @@ clock = pygame.time.Clock()
 
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 block_size = 20
 snake = [(100, 100)]
 direction = (block_size, 0)
+
+def spawn_food():
+    x = random.randrange(0, 600, block_size)
+    y = random.randrange(0, 400, block_size)
+    return (x, y)
+
+food = spawn_food()
 
 running = True
 while running:
@@ -31,11 +40,16 @@ while running:
     head_x, head_y = snake[0]
     new_head = (head_x + direction[0], head_y + direction[1])
     snake.insert(0, new_head)
-    snake.pop()
+    
+    if new_head == food:
+        food = spawn_food()
+    else:
+        snake.pop()
     
     screen.fill(BLACK)
     for segment in snake:
         pygame.draw.rect(screen, GREEN, (segment[0], segment[1], block_size, block_size))
+    pygame.draw.rect(screen, RED, (food[0], food[1], block_size, block_size))
     pygame.display.update()
     clock.tick(10)
 
